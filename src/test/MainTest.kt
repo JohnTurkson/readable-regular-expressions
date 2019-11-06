100bf753c52b9cc6c516673d6a139afa11959f4e
John Turkson <github@johnturkson.com>
2019-11-06 13:44:04 -0800

    Initial commit

---
+++ src/test/MainTest.kt
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class MainTest {
+    @BeforeEach
+    fun setUp() {
+        
+    }
+    
+    @AfterEach
+    fun tearDown() {
+        
+    }
+    
+    @Test
+    fun main() {
+        println("Hello World")
+    }
+}
