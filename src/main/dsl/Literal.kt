4f8f3329261618c492faea0b1d13743e48a68e04
ginabolo <j6p0b@ugrad.cs.ubc.ca>
2019-11-09 23:35:24 -0800

    Address review feedback

--- src/main/dsl/Literal.kt
+++ src/main/dsl/Literal.kt
@@ -1,14 +1,9 @@
 package dsl
 
-import exceptions.RegexSyntaxException
-
 class Literal(private val characters : String) : Group() {
     
     override fun render(builder: StringBuilder) {
-        if (characters.isEmpty()) {
-            throw RegexSyntaxException("Literal should not be empty.")
-        } else {
-            builder.append(characters)
-        }
+        require (characters.isEmpty()) {"Literal should not be empty."}
+        builder.append(characters)
     }
 }
