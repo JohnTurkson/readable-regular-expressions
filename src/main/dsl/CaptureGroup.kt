4204d2a65f714cce2d8916736db67d6d816388e4
John Turkson <github@johnturkson.com>
2019-11-10 19:46:33 -0800

    Fixed CaptureGroup generating wrong Regex string

--- src/main/dsl/CaptureGroup.kt
+++ src/main/dsl/CaptureGroup.kt
@@ -2,6 +2,6 @@ package dsl
 
 class CaptureGroup : Group() {
     override fun toString(): String {
-        return children.joinToString(separator = "|", prefix = "(", postfix = ")")
+        return children.joinToString(separator = "", prefix = "(", postfix = ")")
     }
 }
