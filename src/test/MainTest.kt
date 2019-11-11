b21a86761d1176f1b1909227f9f3946b1c4621e7
John Turkson <github@johnturkson.com>
2019-11-10 19:01:13 -0800

    Removed tests

--- src/test/MainTest.kt
+++ src/test/MainTest.kt
@@ -2,7 +2,6 @@ import dsl.regex
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
 
 internal class MainTest {
     
@@ -18,14 +17,6 @@ internal class MainTest {
     
     @Test
     fun testOptional() {
-        // Literal cannot contain optional, optional must contain a single group type object cases are covered at compile-time
-        val sb = StringBuilder()
-        regex {
-            optional {
-                literal { "a" }
-            }
-        }.render(sb)
-        assertEquals("a?", sb.toString())
+        
     }
-
 }
