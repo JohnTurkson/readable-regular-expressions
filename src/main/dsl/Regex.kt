ee75debb33ebda975ffe32e14c689a4d4cf141cd
John Turkson <github@johnturkson.com>
2019-11-10 19:09:22 -0800

    Updated Regex class to include new Groups

--- src/main/dsl/Regex.kt
+++ src/main/dsl/Regex.kt
@@ -1,28 +1,49 @@
 package dsl
 
-class Regex : Node() {
-    val expressions = arrayListOf<Node>()
+open class Regex {
+    internal val children: MutableList<Regex> = mutableListOf()
     
-    override fun render(builder: StringBuilder) {
-        // TODO
-        for (e in expressions) {
-            e.render(builder)
-        }
+    override fun toString(): String {
+        return children.joinToString(separator = "")
     }
     
-    fun optional(init: () -> Group): Optional {
-        val node = Optional(init())
-        expressions.add(node)
-        return node
+    fun toRegex(): kotlin.text.Regex {
+        return Regex(this.toString())
     }
     
-    fun literal(init: () -> String): Literal {
-        return Literal(init())
+    fun literal(init: () -> String): Regex {
+        val literal = Literal(init())
+        this.children.add(literal)
+        return this
+    }
+    
+    fun oneOf(init: OneOf.() -> Regex): Regex {
+        val oneOf = OneOf().init()
+        this.children += oneOf
+        return this
+    }
+    
+    fun captureGroup(init: CaptureGroup.() -> Regex): Regex {
+        val captureGroup = CaptureGroup().init()
+        require(captureGroup.children.size == 1) { "capture groups can only contain one group" }
+        this.children.add(captureGroup)
+        return this
+    }
+    
+    fun regex(init: Regex.() -> Regex): Regex {
+        val regex = Regex().init()
+        this.children.add(regex)
+        return this
+    }
+    
+    fun optional(init: Optional.() -> Regex): Regex {
+        val optional = Optional().init()
+        require(optional.children.size == 1) { "optional blocks can only contain one group" }
+        this.children.add(optional)
+        return this
     }
 }
 
-fun regex(init: Regex.() -> Unit): Regex {
-    val node = Regex()
-    node.init()
-    return node
+fun regex(init: Regex.() -> Regex): Regex {
+    return Regex().init()
 }
