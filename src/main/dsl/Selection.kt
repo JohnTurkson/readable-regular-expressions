6978f68303f563a83ca520088ba0d39bd4429321
John Turkson <github@johnturkson.com>
2019-11-20 13:05:50 -0800

    TODO toString() for Selection

--- src/main/dsl/Selection.kt
+++ src/main/dsl/Selection.kt
@@ -3,33 +3,12 @@ package dsl
 class Selection : Group() {
     
     override fun toString(): String {
-        var result = ""
-        
-        if (positiveCharacters.isNotEmpty()) {
-            result += positiveCharacters.joinToString(separator = "", prefix = "[", postfix = "]")
-        }
-        
-        if (negativeCharacters.isNotEmpty()) {
-            if (result.isNotEmpty()) result += "|"
-            result += negativeCharacters.joinToString(separator = "", prefix = "[^", postfix = "]")
-        }
-        
-        if (positiveRanges.isNotEmpty()) {
-            if (result.isNotEmpty()) result += "|"
-            result += positiveRanges.joinToString(
-                    separator = "",
-                    prefix = "[",
-                    postfix = "]"
-            ) { "${it.first}-${it.last}" }
-        }
-        
-        if (negativeRanges.isNotEmpty()) {
-            if (result.isNotEmpty()) result += "|"
-            result += negativeRanges.joinToString(
-                    separator = "",
-                    prefix = "[^",
-                    postfix = "]"
-            ) { "${it.first}-${it.last}" }
+        for (g in groups) {
+            when (g) {
+                is Character -> g.toString()
+                
+                // TODO
+            }
         }
         
         return ""
